0451132a681684aad64d71dbc0ae7326705f3622
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-06-08 05:24:43 +0530

    Update SetMatZero.cpp

--- Arrays-1/SetMatZero.cpp
+++ Arrays-1/SetMatZero.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
 #include <vector>
 
-void setZeros(vector<vector<int>> &matrix)
-{
-	int n = matrix.size(), m = matrix[0].size();
-	int row = 1, col = 1;
-	for(int i=0; i<n; i++){
-		if(matrix[i][0]==0){ col=0; break;}
-	}
-	for(int i=0; i<m; i++){
-		if(matrix[0][i]==0){ row=0; break;}
-	}
-	for(int i=1; i<n; i++){
-		for(int j=1; j<m; j++){
-			if(matrix[i][j]==0){
-				matrix[i][0] = 0;
-				matrix[0][j] = 0;
-			}
-		}
-	}
-	for(int i=1; i<n; i++){
-		for(int j=1; j<m; j++){
-			if(matrix[i][0]==0 || matrix[0][j]==0){
-				matrix[i][j]=0;
-			}
-		}
-	}
-	if(col==0){
-		for(int i=0; i<n; i++){
-			matrix[i][0]=0;
-		}
-	}
-	if(row==0){
-		for(int i=0; i<m; i++){
-			matrix[0][i]=0;
-		}
-	}
+void setZeroes(vector<vector<int> > &matrix) {
+    int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+
+    for (int i = 0; i < rows; i++) {
+        if (matrix[i][0] == 0) col0 = 0;
+        for (int j = 1; j < cols; j++)
+            if (matrix[i][j] == 0)
+                matrix[i][0] = matrix[0][j] = 0;
+    }
+
+    for (int i = rows - 1; i >= 0; i--) {
+        for (int j = cols - 1; j >= 1; j--)
+            if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                matrix[i][j] = 0;
+        if (col0 == 0) matrix[i][0] = 0;
+    }
 }
