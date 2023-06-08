5aa8d55f4b9e9c0bbe761c5f9bdcf22a2c6aeb06
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-06-08 16:16:56 +0530

    Update Kadanes.cpp

--- Arrays-1/Kadanes.cpp
+++ Arrays-1/Kadanes.cpp
@@ -1,4 +1,9 @@
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+//Kadane's algo
 long long maxSubarraySum(int arr[], int n)
 {
     long long res = LONG_MIN, sum = 0;
@@ -8,4 +13,17 @@ long long maxSubarraySum(int arr[], int n)
         if(sum<0) sum=0;
     }
     return res>0?res:0;
+}
+int main() {
+    // Example usage
+    int arr[] = {1, -2, 3, 4, -5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    // Compute the maximum subarray sum
+    long long maxSum = maxSubarraySum(arr, size);
+
+    // Print the result
+    cout << "Maximum subarray sum: " << maxSum << endl;
+
+    return 0;
 }
