0ab20209b2ebc1f6cb3075911ab74b006775483b
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-06-08 20:09:16 +0530

    Create BFS.cpp

---
+++ Graph/BFS.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+void breadthFirstSearch(int ind, vector<int> &vis, vector<int> &temp, vector<int> adjList[]){
+    queue<int> q;
+    q.push(ind);
+    while(!q.empty()){
+        int curr = q.front();
+        temp.push_back(curr);
+        vis[curr] = 1;
+        q.pop();
+        for(auto it: adjList[curr]){
+            if(!vis[it]){
+                q.push(it);
+            }
+        }
+    }
+}
+vector<int> BFS(int V, vector<pair<int, int>> edges)
+{
+    //create adjacency list
+    vector<int> adjList[V];
+    for(int i=0; i<edges.size(); i++){
+        adjList[edges[i].first].push_back(edges[i].second);
+        adjList[edges[i].second].push_back(edges[i].first);
+    }
+    vector<int> res;
+    vector<int> vis(V, 0);
+    for(int i=0; i<V; i++){
+      if (!vis[i]) {
+        breadthFirstSearch(i, vis, res, adjList);
+      }
+    }
+    return res;
+}
