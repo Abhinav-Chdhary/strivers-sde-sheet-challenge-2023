400724db10b237f6bd1a8d33ef87678ff7f0c243
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-06-08 16:32:37 +0530

    Update Stocks1.cpp

--- Arrays-1/Stocks1.cpp
+++ Arrays-1/Stocks1.cpp
@@ -1,10 +1,29 @@
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
 int maximumProfit(vector<int> &prices){
     int maxProfit = 0, minPrice = INT_MAX;
+    
     for(int i=0; i<prices.size(); i++){
         minPrice = min(minPrice, prices[i]);
         int profitToday = prices[i]-minPrice;
         maxProfit = max(maxProfit, profitToday);
     }
     return maxProfit;
+}
+
+int main() {
+    // Example usage
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    // Compute the maximum profit
+    int maxProfit = maximumProfit(prices);
+
+    // Print the result
+    cout << "Maximum Profit: " << maxProfit << endl;
+
+    return 0;
 }
