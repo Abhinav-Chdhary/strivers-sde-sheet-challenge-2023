3ad3956b422dacc2eb4bf09205e66f8f517dedaa
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-06-25 11:53:14 +0530

    Next problems in stack

---
+++ StackandQueue/NextGreaterElement.cpp
@@ -0,0 +1,12 @@
+vector<int> nextGreater(vector<int> &arr, int n) {
+    vector<int> ans(n, -1);
+    stack<int> stk;
+    for(int i=n-1; i>=0; i--){
+        if(stk.empty()) ans[i] = -1;
+        while(!stk.empty() && stk.top()<=arr[i]) stk.pop();
+        if(!stk.empty()) ans[i] = stk.top();
+        stk.push(arr[i]);
+    }
+    return ans;
+
+}
