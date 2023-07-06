3cbb881ebfa380d5b41d823ef6b2f7354e309d74
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-06 12:02:22 +0530

    Create MaxConsec1s.cpp

---
+++ LinkList&Arrays/MaxConsec1s.cpp
@@ -0,0 +1,16 @@
+//Sliding window approach
+//Intuition gud af
+int longestSubSeg(vector<int> &nums , int n, int k){
+    int i=0, j=0;
+        while(i<nums.size()){
+            if(nums[i]==0){
+                k--;
+            }
+            if(k<0){
+                if(nums[j]==0) k++;
+                j++;
+            }
+            i++;
+        }
+        return i-j;
+}
