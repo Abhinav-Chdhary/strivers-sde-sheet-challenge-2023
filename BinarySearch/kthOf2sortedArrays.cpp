6f25605c905c63d0befb222b639f04e13402032a
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-15 17:03:05 +0530

    Update kthOf2sortedArrays.cpp

--- BinarySearch/kthOf2sortedArrays.cpp
+++ BinarySearch/kthOf2sortedArrays.cpp
@@ -26,3 +26,30 @@ int ninjaAndLadoos(vector<int> &row1, vector<int> &row2, int m, int n, int k) {
     return -1;
 }
 //optimal using binary search O(log(n+m))
+int ninjaAndLadoos(vector<int> &row1, vector<int> &row2, int m, int n, int k) {
+    if(m>n) return ninjaAndLadoos(row2, row1, n, m, k);
+    long l = max(0, k-n), r = min(k, m);
+    while(l<=r){
+        int p1 = l+(r-l)/2; //partition of smaller array
+        int p2 = k-p1; //partition of bigger array
+        //if all elements of 'a' are greater than all elements of 'b'
+        //then set l1 = -inf else 
+        int l1 = p1==0? INT_MIN: row1[p1-1];
+        //similarly for p2 partition
+        int l2 = p2==0? INT_MIN: row2[p2-1];
+        //if all elements of 'a' are less than all elements of 'b'
+        //then set r1 = +inf else 
+        int r1 = p1==m? INT_MAX: row1[p1];
+        //similarly for p2 partition
+        int r2 = p2==n? INT_MAX: row2[p2];
+
+        if(l1<=r2 && l2<=r1){
+            return max(l1, l2);
+        }
+        else if(l1>r2)
+            r = p1-1;
+        else
+            l = p1+1;
+    }
+    return 1;
+}
