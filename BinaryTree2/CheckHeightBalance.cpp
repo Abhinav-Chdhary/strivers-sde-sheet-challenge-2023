61c788e537af645ca256aacedfb3e83998594a99
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-15 21:06:28 +0530

    Update CheckHeightBalance.cpp

--- BinaryTree2/CheckHeightBalance.cpp
+++ BinaryTree2/CheckHeightBalance.cpp
@@ -9,3 +9,25 @@ int traverse(BinaryTreeNode<int>* root){
 bool isBalancedBT(BinaryTreeNode<int>* root) {
     return traverse(root)!=-1;
 }
+ bool isBalanced(TreeNode* root) {
+        if(!root) return true;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* cur = q.front();
+            q.pop();
+            if(cur->left) q.push(cur->left);
+            if(cur->right) q.push(cur->right);
+            int left = height(cur->left);
+            int right = height(cur->right);
+            if(abs(left-right)>1)
+                return false;
+        }
+        return true;
+    }
+    int height(TreeNode* root){
+        if(!root) return true;
+        int left = height(root->left);
+        int right = height(root->right);
+        return 1+max(left, right);
+    }
