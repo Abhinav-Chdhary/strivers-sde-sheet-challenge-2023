6a2e5bd09fe443a3f0c28769ec7aca843691abba
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-15 17:04:07 +0530

    Update LongestCommonPrefix.cpp

--- String/LongestCommonPrefix.cpp
+++ String/LongestCommonPrefix.cpp
@@ -1,67 +1,17 @@
-#include <bits/stdc++.h> 
-struct Node{
-    Node* links[26];
-    bool flag = false;
-    bool hasKey(char ch){
-        return (links[ch-'a']!=NULL);
-    }
-    void put(char ch, Node* node){
-        links[ch-'a'] = node;
-    }
-    Node* get(char ch){
-        return links[ch-'a'];
-    }
-    void setEnd(){
-        flag = true;
-    }
-    bool isEnd(){
-        return flag;
-    }
-};
-class Trie {
-    Node* root;
-public:
+#include <algorithm>
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    sort(arr.begin(), arr.end());
+    string first = arr[0];
+    string last = arr[n - 1];
 
-    Trie() {
-        root = new Node();
+    string result = "";
+    for (int i = 0; i < first.length(); i++)
+    {
+        if (first[i] == last[i])
+            result += first[i];
+        else
+            break;
     }
-
-    /** Inserts a word into the trie. */
-    void insert(string word) {
-        Node* node = root;
-        for(int i=0; i<word.length(); i++){
-            if(!node->hasKey(word[i])){
-                node->put(word[i], new Node());
-            }
-            node=node->get(word[i]);
-        }
-        node->setEnd();
-    }
-
-    /** Returns if the word is in the trie. */
-    bool search(string word) {
-        Node* node = root;
-        for(int i=0; i<word.length(); i++){
-			node = node->get(word[i]);
-            if(!node->isEnd()) return false;
-        }
-        return true;
-    }
-};
-string completeString(int n, vector<string> &a){
-    Trie* trie = new Trie();
-    for(int i=0; i<n; i++){
-        trie->insert(a[i]);
-    }
-    string ans="";
-    for(auto s: a){
-        if(trie->search(s)){
-            if(s.size()>ans.size())
-                ans = s;
-            else if((s.size()==ans.size() && s<ans))
-                ans = s;
-        }
-    }
-    if(ans.size()==0) return "None";
-    return ans;
+    return result;
 }
