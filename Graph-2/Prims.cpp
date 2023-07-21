b0d73699c9d35e71f9425b396e49791a77e54ce1
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-21 09:49:21 +0530

    Update Prims.cpp

--- Graph-2/Prims.cpp
+++ Graph-2/Prims.cpp
@@ -21,3 +21,27 @@ int spanningTree(int V, vector<vector<int>> adj[])
         }
         return sum;
     }
+//Another way of doing it
+static bool comp(const pair<pair<int, int>, int> &a, const pair<pair<int, int>, int> &b){
+    return a.second<=b.second;
+}
+vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
+{
+    vector<int> vis(n+1, 0);
+    vector<pair<pair<int, int>, int>> result;
+    sort(g.begin(), g.end(), comp);
+    int count=0;
+    for(int i=0; i<m && count<n; i++){
+        int u = g[i].first.first;
+        int v = g[i].first.second;
+        int wt = g[i].second;
+        if(u==v) continue;
+        if(vis[u] && vis[v]) continue;
+        else{
+            result.push_back(g[i]);
+            vis[u]=1;
+            vis[v]=1;
+        }
+    }
+    return result;
+}
