e3035f2e6fdc86c9773babfbcefef6e1f3d14a93
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-15 15:35:36 +0530

    Update MaxXOR.cpp

--- Trie/MaxXOR.cpp
+++ Trie/MaxXOR.cpp
@@ -13,3 +13,69 @@ int maximumXor(vector<int> A)
     return ans;
 }
 //Optimal using trie
+class Node{
+    public:
+    Node *one, *zero;
+};
+class Trie{
+    Node* root = new Node();
+    public:
+    void insert(int num){
+        Node* curr = root;
+        int bitInd = 30;
+        while(bitInd>=0){
+            int mask = 1<<bitInd;
+            if(num&mask){
+                if(!curr->one)
+                    curr->one = new Node();
+                curr = curr->one;
+            }
+            else{
+                if(!curr->zero)
+                    curr->zero = new Node();
+                curr = curr->zero;
+            }
+            bitInd--;
+        }
+    }
+    int query(int find){
+        Node* curr = root;
+        int ans = 0, bitInd = 30;
+        while(bitInd>=0){
+            int mask = 1<<bitInd;
+            if(find&mask){
+                if(curr->one){
+                    curr = curr->one;
+                    ans |= mask;
+                }
+                else if(curr->zero) curr = curr->zero;
+            }
+            else{
+                if(curr->zero)
+                    curr = curr->zero;
+                else if(curr->one){ 
+                    curr = curr->one;
+                    ans |= mask;
+                }
+            }
+            bitInd--;
+        }
+        return ans;
+    }
+};
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie *trie = new Trie();
+        int ans = 0, n= nums.size();
+        for(int i=0; i<n; i++){
+            trie->insert(nums[i]);
+        }
+        for(int i=0; i<n; i++){
+            int find = INT_MAX ^ nums[i];
+            int b = trie->query(find);
+            ans = max(ans, nums[i]^b);
+        }
+        return ans;
+    }
+};
